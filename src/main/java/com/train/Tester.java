03b374ea98f663ce02641f47c350c8e88ca5e3e9
CKM <CKM>
2019-02-24 20:14:31 +0800

    Homwork 2

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -1,17 +1,32 @@
 package com.train;
 
+import sun.font.TrueTypeFont;
+
 import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        int numberOfTicket;
-        int numberOfRTTicket;
-        System.out.print("Please enter number of tickets: ");
+        int sumOfTicket = 0;
+        int sumOfRTTicket = 0;
+        int tmp = 0;
         Scanner scanner = new Scanner(System.in);
-        numberOfTicket = scanner.nextInt();
-        System.out.print("How many round-trip tickets: ");
-        numberOfRTTicket = scanner.nextInt();
-        Ticket tic = new Ticket(numberOfTicket, numberOfRTTicket);
+        do {
+            System.out.print("Please enter number of tickets: ");
+            tmp = scanner.nextInt();
+            if (tmp != -1) {
+                sumOfTicket += tmp;
+            } else {
+                break;
+            }
+            System.out.print("How many round-trip tickets: ");
+            tmp = scanner.nextInt();
+            if (tmp != -1) {
+                sumOfRTTicket += tmp;
+            } else {
+                break;
+            }
+        } while (true);
+        Ticket tic = new Ticket(sumOfTicket, sumOfRTTicket);
         tic.print();
     }
 }
